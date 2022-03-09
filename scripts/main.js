0d90555f15fdebd2d78d47bf81f69d440dcac38c
Ericbuis <7462601@isd535.org>
2022-03-08 19:42:36 -0600

    hehe

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
-// Put your JavaScript here
+function setDetails(imageUrl, titleText) {
+    'use strict';
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.scroll.setAttribute('src' , imageUrl);
+
+    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttritube('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttritube('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
